51fbdf541dd16322908e7eacb00c4a4779822c06
Minakshi Verma <mvedwan@gmail.com>
2020-02-20 15:43:41 -0500

    "Created ProductContext"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import {ProductContext} from './contexts/ProductContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -13,28 +14,34 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		return setCart([...cart, item])
 	};
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value ={{products, addItem}}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+				component ={Products} />
+
+				{/* render={() => (
+				 	<Products
+				 		products={products}
+					addItem={addItem}
+				 	/>
+				 )} */}
+			 
 
 			<Route
 				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+				// render={() => <ShoppingCart cart={cart} />}
+				component ={ShoppingCart}
 			/>
+			</ProductContext.Provider>
 		</div>
 	);
 }
